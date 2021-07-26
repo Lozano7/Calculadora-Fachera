0089cd3fa1a4c8f2de2847f9725771c6910fb361
Lozano7 <56984985+Lozano7@users.noreply.github.com>
2021-07-26 17:54:06 -0500

    Themas terminados y funcionando

---
+++ js/code.js
@@ -0,0 +1,31 @@
+const tema = document.querySelectorAll(".cambiarTheme");
+const links = document.getElementsByTagName("link");
+const tema1 = document.getElementById("theme1");
+const tema2 = document.getElementById("theme2");
+const tema3 = document.getElementById("theme3");
+function encontrarLink(link, theme) {
+  for (let i = 0; i < link.length; i++) {
+    if (link[i].title == "cambios") {
+      link[i].href = "css/" + theme + ".css";
+    }
+  }
+}
+if (
+  tema1.checked == false &&
+  tema2.checked == false &&
+  tema3.checked == false
+) {
+  encontrarLink(links, "theme1");
+}
+tema.forEach((radioButon) => {
+  console.log(radioButon.id);
+  radioButon.addEventListener("change", () => {
+    if (radioButon.id == "theme1") {
+      encontrarLink(links, "theme1");
+    } else if (radioButon.id == "theme2") {
+      encontrarLink(links, "theme2");
+    } else if (radioButon.id == "theme3") {
+      encontrarLink(links, "theme3");
+    }
+  });
+});

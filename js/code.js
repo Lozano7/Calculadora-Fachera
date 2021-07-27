5224c350386907296706199992132aedd9796e4b
Lozano7 <56984985+Lozano7@users.noreply.github.com>
2021-07-26 23:06:47 -0500

    Calculadora Funcional

--- js/code.js
+++ js/code.js
@@ -3,6 +3,15 @@ const links = document.getElementsByTagName("link");
 const tema1 = document.getElementById("theme1");
 const tema2 = document.getElementById("theme2");
 const tema3 = document.getElementById("theme3");
+const del = document.getElementById("del");
+const botonesNum = document.querySelectorAll(".num");
+const botonesFunc = document.querySelectorAll(".function");
+const pantalla = document.getElementById("pantallaMos");
+const reset = document.getElementById("reset");
+const igual = document.getElementById("igual");
+var opeActual = "";
+var opeAnterior = "";
+var operacion = undefined;
 function encontrarLink(link, theme) {
   for (let i = 0; i < link.length; i++) {
     if (link[i].title == "cambios") {
@@ -26,3 +35,74 @@ tema.forEach((radioButon) => {
     }
   });
 });
+reset.addEventListener("click", () => {
+  pantalla.value = null;
+  operacion = undefined;
+  opeActual = "";
+  opeAnterior = "";
+});
+botonesNum.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    if (pantalla.value.length <= 20) {
+      agregarNumero(boton.innerHTML);
+    }
+  });
+});
+botonesFunc.forEach((boton) => {
+  boton.addEventListener("click", () => {
+    selectOperacion(boton.innerHTML);
+  });
+});
+del.addEventListener("click", () => {
+  let numero = pantalla.value;
+  let numeroRecortado = numero.substring(0, numero.length - 1);
+  opeActual = numeroRecortado;
+  actulizarDisplay();
+});
+
+igual.addEventListener("click", () => {
+  calcular();
+  actulizarDisplay();
+});
+
+function agregarNumero(numero) {
+  opeActual = opeActual.toString() + numero.toString();
+  actulizarDisplay();
+}
+function actulizarDisplay() {
+  pantalla.value = opeActual;
+}
+function selectOperacion(operacionBoton) {
+  if (opeActual === "") return;
+  if (opeAnterior !== "") {
+    calcular();
+  }
+  operacion = operacionBoton.toString();
+  opeAnterior = opeActual;
+  opeActual = "";
+}
+function calcular() {
+  var calculo;
+  const anterior = parseFloat(opeAnterior);
+  const actual = parseFloat(opeActual);
+  if (isNaN(anterior) || isNaN(actual)) return;
+  switch (operacion) {
+    case "+":
+      calculo = anterior + actual;
+      break;
+    case "-":
+      calculo = anterior - actual;
+      break;
+    case "x":
+      calculo = anterior * actual;
+      break;
+    case "/":
+      calculo = anterior / actual;
+      break;
+    default:
+      return;
+  }
+  opeActual = calculo;
+  operacion = undefined;
+  opeAnterior = "";
+}
